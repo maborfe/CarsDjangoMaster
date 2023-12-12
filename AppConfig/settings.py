33ccc7378aaeb9a16f5f91ca911bff819748b4b7
Marcell Borges <marcell.borges@gmail.com>
2023-12-11 21:29:04 -0300

    versao final, trocando para postgres

--- AppConfig/settings.py
+++ AppConfig/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'AppConfig.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'carros',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'mbf',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
